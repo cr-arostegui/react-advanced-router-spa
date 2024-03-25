ad9fc9081bb534ee6a313f3d428650c91e55b069
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-03-24 21:12:47 -0400

    Extracting error data & throwing Responses

--- frontend/src/pages/ErrorPage.js
+++ frontend/src/pages/ErrorPage.js
@@ -1,5 +1,23 @@
+import { useRouteError } from 'react-router-dom';
+import PageContent from '../components/PageContent';
+
 export default function ErrorPage() {
+  const error = useRouteError();
+  let title = 'An error occurred!';
+  let message = 'Something went wrong!';
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = 'Not found!';
+    message = 'Could not find resource or page';
+  }
+
   return (
-    <h1>An error occurred</h1>
-  )
+    <PageContent title={title}>
+      <p>{message}</p>
+    </PageContent>
+  );
 }
