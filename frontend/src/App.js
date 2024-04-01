859c102a39f85a082b74d3f81e2f25a58ee27797
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-04-01 11:11:35 -0400

    Working with action() function, create new event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import EventsPage, { loader as eventsLoader } from './pages/EventsPage';
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from './pages/EventDetailPage';
-import NewEventPage from './pages/NewEventPage';
+import NewEventPage, { action as newEventAction } from './pages/NewEventPage';
 import EditEventPage from './pages/EditEventPage';
 import ErrorPage from './pages/ErrorPage';
 // Challenge / Exercise
@@ -60,7 +60,7 @@ const router = createBrowserRouter([
               { path: 'edit', element: <EditEventPage /> },
             ],
           },
-          { path: 'new', element: <NewEventPage /> },
+          { path: 'new', element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
